ed79d70d7e7a4dbc756c1796e1c533e30648defd
Daniel F. Dickinson <cshored@danielfdickinson.ca>
2021-03-24 21:46:58 -0400

    Add HTML and hugo build success check scripts
    Signed-off-by: Daniel F. Dickinson <cshored@danielfdickinson.ca>

---
+++ tests/scripts/t01000-tidy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+
+OTS=SUCCESS
+
+htfiles="$(find public -name '*.html' -print)"
+
+for htfile in $htfiles; do
+	if grep -q 'meta http-equiv="refresh' $htfile; then
+		echo "SKIP: Linting (tidy) for $htfile; it's an alias (meta refresh only)"
+	else
+		tidy -o /dev/null -q --drop-empty-elements no --indent auto $htfile
+		RC=$?
+		if [ "$RC" = "2" ]; then
+			echo "FAIL: Linting (tidy) for $htfile"
+			exit 3
+		fi
+	fi
+done
+
+echo "PASS: Linting (tidy)"
+
+exit 0
