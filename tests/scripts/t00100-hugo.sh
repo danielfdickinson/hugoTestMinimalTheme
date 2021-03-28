3c8a44b0980a51bd241bce1876a61fb195706696
Daniel F. Dickinson <cshored@danielfdickinson.ca>
2021-03-27 23:44:59 -0400

    Use devel 'development' environment to test tables
    Signed-off-by: Daniel F. Dickinson <cshored@danielfdickinson.ca>

--- tests/scripts/t00100-hugo.sh
+++ tests/scripts/t00100-hugo.sh
@@ -5,7 +5,7 @@ TS=PASS
 if [ -n "$(command -v hugo-basic)" ]; then
 	hugo-basic --path-warnings || TS=FAIL
 elif [ -n "$(command -v hugo-extended)" ]; then
-	hugo-extended --path-warnings --environment="testing" || TS=FAIL
+	hugo-extended --path-warnings --environment="development" || TS=FAIL
 else
 	hugo --path-warnings || TS=FAIL
 fi
